c4d4fe69d2dfc1b7a9e90acc3805c3e5348d5ac5
Abdelrhman <123353482+abdomohsen228@users.noreply.github.com>
2024-03-05 00:06:33 +0200

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_application_1/gardieant.dart';
+void main()
+{
+  runApp(
+  const MaterialApp(
+    home:Scaffold(
+      body: GradientContainer(
+        Color.fromARGB(255, 33, 5, 109),
+         Color.fromARGB(255, 68, 21, 149),
+      ),
+    ),
+  ),
+  );
+}
+
+
+
+
+
+
+
+
